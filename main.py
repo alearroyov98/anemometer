b07e8d653fbc7d62eca4afde6abadaaa9a2955eb
alearroyov98 <alearroyov98@gmail.com>
2019-12-10 11:19:43 -0600

    It's measuring in m/s :D

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import serial
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(5,GPIO.IN)
+counter = 0
 previous = 0
 seconds = 0
+flag = 0
 # ser = serial.Serial(
 #   
 #     port='/dev/ttyS0',
@@ -16,17 +18,17 @@ seconds = 0
 #     timeout=1
 # )
 def falling_edge(channel):
-    global previous
-    global seconds
-    print (seconds)    
+    global flag
+    global current
+    flag = 1
+#     print (seconds)    
     current = time.time()
-    seconds = current - previous
-    previous = current
+#     seconds = current - previous
+#     previous = current
     
 # when a falling edge is detected on port 5, regardless of whatever   
 # else is happening in the program, the function falling_edge will be run  
 GPIO.add_event_detect(5, GPIO.FALLING, callback=falling_edge, bouncetime=300) 
-
 # while a < 200:
 #     if GPIO.input(5):
 #         print("HIGH")
@@ -38,7 +40,18 @@ GPIO.add_event_detect(5, GPIO.FALLING, callback=falling_edge, bouncetime=300)
 # counter=0
 # clear=b'\xFE\x01'
 # ser.write(clear)
-# while 1:
+while 1:
+    if flag == 1:
+        seconds = seconds + (current - previous)
+        previous = current
+        counter = counter + 1
+        flag = 0
+    if counter == 10:
+        seconds = seconds/counter
+        speed = (1/seconds)*2.5*0.44704
+        print("%4.2f m/s" % speed)
+        seconds = 0
+        counter = 0
 #     ser.write(clear)
 #     ser.write(('Write counter:  %d'%(counter)).encode('utf-8'))
 #     time.sleep(1)
